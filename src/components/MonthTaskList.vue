43b4234b8826e201ae37aaa7f2d71292f02d74b5
Bethan Williams <bethanw10@gmail.com>
2025-05-31 21:12:46 +0100

    Update monthly task list

--- src/components/MonthTaskList.vue
+++ src/components/MonthTaskList.vue
@@ -11,18 +11,17 @@
           <Button @click="jumpToThisMonth" :disabled="month == currentMonth" label="Today" size="small" outlined rounded title="Jump to this month"></Button>
         </div>
 		  </div>
-	    <div v-for="task in tasksForMonth(tasks, month)" 
-          :key="task.name" 
-          class="task-list-item" 
-	        :style="{ borderLeft: '4px solid #' + sectionForMonth(task, month)!.color }" >
+	    <div v-for="task in tasksForMonth(tasks, month)" :key="task.name" >
 	      <h3>{{ task.name }}</h3>
-	      <div v-if="!!sectionForMonth(task, month)" class="task-section" >
-	        <b>{{ sectionForMonth(task, month)!.note }} 
-	          <Tag class="month-range" severity="secondary">
-	            {{ monthRange(task, month) }}
-	          </Tag>
-	        </b>
-          <p class="description">{{sectionForMonth(task, month)!.description}}</p>
+	      <div class="task-section" >
+          <div v-for="section in sectionForMonth(task, month)" :style="{ borderLeft: '4px solid #' + section.color }" class="task-list-item" >
+            <b>{{ section.note }} 
+              <Tag class="month-range" severity="secondary">
+                {{ monthRange(section, month) }}
+              </Tag>
+            </b>
+            <p class="description">{{section.description}}</p>
+          </div>
 	      </div>
 	    </div>
 	  </div>
@@ -49,8 +48,7 @@ const currentMonth = computed(() => {
   return date.getMonth();
 })
 
-function monthRange(task: Task, month: number) {
-  const section = sectionForMonth(task, month);
+function monthRange(section: Section, month: number) {
   const startMonth = months[section!.monthStart];
   const endMonth = months[section!.monthEnd];
 
@@ -79,13 +77,17 @@ function tasksForMonth(tasks: Task[], month: number) {
 }
 
 function sectionForMonth(task: Task, month: number) {
-  return task.rows.flatMap(s => s.sections).find((s) =>
+  return task.rows.flatMap(s => s.sections).filter((s) =>
       s.monthStart <= month && 
       s.monthEnd >= month);
 }
 </script>
 
 <style scoped>
+h3 {
+  font-family: 'Montserrat', sans-serif;
+}
+
 .month-title {
   width: 3rem;
   text-align: center;
@@ -137,10 +139,12 @@ function sectionForMonth(task: Task, month: number) {
   /* border: 1px solid #ccc; */
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px -1px rgba(0, 0, 0, 0.1);;
   background: white;
+  margin: 1rem 0;
+  padding: 1rem;
 }
 
 .task-section {
-  margin: 0.5rem 0;
+  margin: 0 0 2rem 0;
 }
 
 .month-range {
@@ -151,5 +155,6 @@ function sectionForMonth(task: Task, month: number) {
   font-size: 0.9rem;
   color: #555;
   white-space: pre-line;
+  margin: 0.5em 0 0 0;
 }
 </style>
