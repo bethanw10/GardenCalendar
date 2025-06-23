386b8094896341ad2acacbf559887004e8948ec3
Bethan Williams <bethanw10@gmail.com>
2025-06-23 14:41:18 +0100

    Remove firebase

--- src/main.ts
+++ src/main.ts
@@ -8,26 +8,8 @@ import App from './App.vue'
 import router from './router'
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD2hVY4Zqh45oz2J6tnWy7IoN8jgcwxU54",
-  authDomain: "garden-calendar-bethanw.firebaseapp.com",
-  projectId: "garden-calendar-bethanw",
-  storageBucket: "garden-calendar-bethanw.firebasestorage.app",
-  messagingSenderId: "830725281174",
-  appId: "1:830725281174:web:5415680b6aa215792db9b7",
-  measurementId: "G-94FJ58JBCV"
-};
-
-// Initialize Firebase
-const fireapp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(fireapp);
 
 //import 'primevue/resources/themes/aura-light-green/theme.css'
 
